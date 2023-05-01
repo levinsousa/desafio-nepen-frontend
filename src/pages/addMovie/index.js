3e1889578fce3ca4c7d0abc0d98c519f1a18bf20
levinsousa <techie.levinsousa@gmail.com>
2023-05-01 16:12:01 -0300

    style and feat: Adicionar e editar filmes

--- src/pages/addMovie/index.js
+++ src/pages/addMovie/index.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import styles from './styles.module.css'
 import api from '../../services/server'
+import HeaderComponent from "../../components/HeaderComponent";
+import { Link } from 'react-router-dom';
 
 export default function AddMovie(){
   const [ title, setTitle ] = useState('')
@@ -17,25 +19,25 @@ export default function AddMovie(){
 
   return(
     <div className={styles.addMovieDiv}>
-      <header className={styles.header}>
-        <span className={styles.headerTitle}> JaVi Pop </span>
-        <div>
-        </div>
-      </header>
+      <HeaderComponent>
+      </HeaderComponent>
 
-      <main className={styles.addMovieForm}>
-        <h1 className={styles.addMovieH1}>Hora de adicionar um novo filme!</h1>
-        <label htmlFor="titleMovie" className={styles.addMovieLabel}>Título do filme</label>
-        <input onChange={(e)=> setTitle(e.target.value)} type="text" id='titleMovie' placeholder='As aventuras de tintin...' className={styles.addMovieInput} />
-        <label htmlFor="durationMovie" className={styles.addMovieLabel}>Duração do filme</label>
-        <input onChange={(e)=> setDuration(e.target.value)} type="text" id='durationMovie' placeholder='1:00:37...' className={styles.addMovieInput} />
-        <label htmlFor="yearMovie" className={styles.addMovieLabel}>Ano de Lançamento</label>
-        <input onChange={(e)=> setYearMovie(e.target.value)} type="number" id='yearMovie' placeholder='2023...' className={styles.addMovieInput} />
-        <label htmlFor="descriptionMovie" className={styles.addMovieLabel}>Descrição do filme</label>
-        <textarea onChange={(e)=> setDescription(e.target.value)} id="descriptionMovie" rows="10" className={styles.addMovieTextarea} />
-        <label htmlFor="urlPoster" className={styles.addMovieLabel}>Link do Poster do filme</label>
-        <input onChange={(e)=> setUrlPoster(e.target.value)} type="text" id='urlPoster' placeholder='https://...' className={styles.addMovieInput} />
-        <button onClick={() => addMovieHandle()} className={styles.addMovieSend}>Adicionar filme</button>
+      <main className={styles.addMovieBody}>
+        <div className={styles.addMovieForm}>
+          <h1 className={styles.addMovieH1}>Hora de adicionar um novo filme!</h1>
+          <label htmlFor="titleMovie" className={styles.addMovieLabel}><h3>Título do filme</h3></label>
+          <input onChange={(e)=> setTitle(e.target.value)} type="text" id='titleMovie' placeholder='As aventuras de tintin...' className={styles.addMovieInput} />
+          <label htmlFor="durationMovie" className={styles.addMovieLabel}><h3>Duração do filme</h3></label>
+          <input onChange={(e)=> setDuration(e.target.value)} type="text" id='durationMovie' placeholder='1:00:37...' className={styles.addMovieInput} />
+          <label htmlFor="yearMovie" className={styles.addMovieLabel}><h3>Ano de Lançamento</h3></label>
+          <input onChange={(e)=> setYearMovie(e.target.value)} type="number" id='yearMovie' placeholder='2023...' className={styles.addMovieInput} />
+          <label htmlFor="descriptionMovie" className={styles.addMovieLabel}><h3>Descrição do filme</h3></label>
+          <textarea onChange={(e)=> setDescription(e.target.value)} id="descriptionMovie" rows="10" className={styles.addMovieTextarea} />
+          <label htmlFor="urlPoster" className={styles.addMovieLabel}><h3>Link do Poster do filme</h3></label>
+          <input onChange={(e)=> setUrlPoster(e.target.value)} type="text" id='urlPoster' placeholder='https://...' className={styles.addMovieInput} />
+          <Link to={'/movies'} onClick={() => addMovieHandle()} className={styles.addMovieSend}>Adicionar filme</Link>
+          <Link to={'/movies'} className={styles.cancelButton}>Cancelar</Link>
+        </div>
       </main>
     </div>
   )
--- src/pages/editMovie/index.js
+++ src/pages/editMovie/index.js
@@ -1,25 +1,58 @@
+import { useEffect,useState } from 'react'
 import styles from './styles.module.css'
+import api from '../../services/server'
+import HeaderComponent from "../../components/HeaderComponent";
+import { Link, useParams } from 'react-router-dom';
+
 
 export default function EditMovie(){
+  const {id} = useParams()
+
+  const [ movie, setMovie ] = useState()
+  const [ title, setTitle ] = useState('')
+  const [ duration, setDuration ] = useState('')
+  const [ yearMovie, setYearMovie ] = useState()
+  const [ description, setDescription ] = useState('')
+  const [ urlPoster, setUrlPoster ] = useState('')
+
+  useEffect(()=>{
+    api.get(`/movie/${id}`).then(res => res.data).then(movie => {
+      setTitle(movie.title)
+      setDuration(movie.duration)
+      setYearMovie(movie.yearMovie)
+      setDescription(movie.description)
+      setUrlPoster(movie.urlPoster)
+    })
+  }, [])
+
+  async function editMovieHandle(){
+    console.log({title, duration, yearMovie, description, urlPoster})
+    await api.patch(`/movie/${id}`, {title, duration, yearMovie, description, urlPoster})
+      .then(e => console.log("Edição realizado com sucesso!"))
+      .catch(error => console.error(error))
+  }
+
   return(
     <div className={styles.editMovieDiv}>
-      <header className={styles.header}>
-        <span className={styles.headerTitle}> JaVi Pop </span>
-        <div>
-        </div>
-      </header>
+      <HeaderComponent>
+      </HeaderComponent>
 
-      <main className={styles.editMovieForm}>
-        <h1 className={styles.editMovieH1}>Precisa editar seu filme?</h1>
-        <label htmlFor="titleMovie" className={styles.editMovieLabel}>Título do filme</label>
-        <input type="text" id='titleMovie' placeholder='As aventuras de tintin...' className={styles.editMovieInput} />
-        <label htmlFor="durationMovie" className={styles.editMovieLabel}>Duração do filme</label>
-        <input type="" id='durationMovie' placeholder='1:00:37...' className={styles.editMovieInput} />
-        <label htmlFor="yearMovie" className={styles.editMovieLabel}>Ano de Lançamento</label>
-        <input type="number" id='yearMovie' className={styles.editMovieInput} />
-        <label htmlFor="descriptionMovie" className={styles.editMovieLabel}>Descrição do filme</label>
-        <textarea id="descriptionMovie" rows="10" className={styles.editMovieTextarea} />
-        <button className={styles.editMovieSend}>Salvar</button>
+      <main className={styles.editMovieBody}>
+        <div className={styles.editMovieForm}>
+          <h1 className={styles.editMovieH1}>Vamos realizar suas modificações agora</h1>
+          <label htmlFor="titleMovie" className={styles.editMovieLabel}><h3>Título do filme</h3></label>
+          <input value={title} onChange={(e)=> setTitle(e.target.value)} type="text" id='titleMovie' placeholder='As aventuras de tintin...' className={styles.editMovieInput} />
+          <label htmlFor="durationMovie" className={styles.editMovieLabel}><h3>Duração do filme</h3></label>
+          <input value={duration} onChange={(e)=> setDuration(e.target.value)} type="text" id='durationMovie' placeholder='1:00:37...' className={styles.editMovieInput} />
+          <label htmlFor="yearMovie" className={styles.editMovieLabel}><h3>Ano de Lançamento</h3></label>
+          <input value={yearMovie} onChange={(e)=> setYearMovie(e.target.value)} type="number" id='yearMovie' placeholder='2023...' className={styles.editMovieInput} />
+          <label htmlFor="descriptionMovie" className={styles.editMovieLabel}><h3>Descrição do filme</h3></label>
+          <textarea value={description} onChange={(e)=> setDescription(e.target.value)} id="descriptionMovie" rows="10" className={styles.editMovieTextarea} />
+          <label htmlFor="urlPoster" className={styles.editMovieLabel}><h3>Link do Poster do filme</h3></label>
+          <input value={urlPoster} onChange={(e)=> setUrlPoster(e.target.value)} type="text" id='urlPoster' placeholder='https://...' className={styles.editMovieInput} />
+          <Link to={'/movies'} onClick={() => editMovieHandle()} className={styles.editMovieSend}>Salvar modificações</Link>
+          <Link to={'/movies'} className={styles.cancelButton}>Cancelar</Link>
+        </div>
       </main>
     </div>
   )
